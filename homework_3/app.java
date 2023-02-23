e25c580bc6beec8d3e04bca152c9df94fe8176ed
name <you@example.com>
2023-02-23 23:14:38 +0300

    hm3

---
+++ homework_3/app.java
@@ -0,0 +1,58 @@
+package Java_homework.homework_3;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class app {
+
+    public static void main(String args[]) {
+        int[] sortArr = fillArray();
+        int[] result = mergeSort(sortArr);
+        String strSortArr = Arrays.toString(sortArr);
+        String strResult = Arrays.toString(result);
+        System.out.printf("Исходный массив %s\n", strSortArr);
+        System.out.printf("Отсортированный массив %s", strResult);
+    }
+
+    public static int[] mergeSort(int[] sortArr) {
+        int[] buffer1 = Arrays.copyOf(sortArr, sortArr.length);
+        int[] buffer2 = new int[sortArr.length];
+        int[] result = mergeSortInner(buffer1, buffer2, 0, sortArr.length);
+        return result;
+    }
+
+    public static int[] mergeSortInner(int[] buffer1, int[] buffer2, int startIndex, int endIndex) {
+        if (startIndex >= endIndex - 1) {
+            return buffer1;
+        }
+
+        int middle = startIndex + (endIndex - startIndex) / 2;
+        int[] sorted1 = mergeSortInner(buffer1, buffer2, startIndex, middle);
+        int[] sorted2 = mergeSortInner(buffer1, buffer2, middle, endIndex);
+
+        int index1 = startIndex;
+        int index2 = middle;
+        int destIndex = startIndex;
+        int[] result = sorted1 == buffer1 ? buffer2 : buffer1;
+        while (index1 < middle && index2 < endIndex) {
+            result[destIndex++] = sorted1[index1] < sorted2[index2]
+                    ? sorted1[index1++] : sorted2[index2++];
+        }
+        while (index1 < middle) {
+            result[destIndex++] = sorted1[index1++];
+        }
+        while (index2 < endIndex) {
+            result[destIndex++] = sorted2[index2++];
+        }
+        return result;
+    }
+
+    public static int[] fillArray() {
+        int[] arr = new int[10];
+        Random rand = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(100);
+        }
+        return arr;
+    }
+}
