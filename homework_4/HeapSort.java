6b959054a21e89f4787f48b3933726edf6202cc0
name <you@example.com>
2023-02-24 18:29:40 +0300

    method to convert to subtree binary heap

--- homework_4/HeapSort.java
+++ homework_4/HeapSort.java
@@ -8,7 +8,30 @@ public class HeapSort{
     public static void main(String args[])
     {
         int arr[] = fillArray();
-      
+        HeapSort ob = new HeapSort();
+       
+    }
+
+
+    }
+
+     void heapify(int arr[], int n, int i)
+    {
+        int largest = i;
+        int left = 2*i + 1; 
+        int right = 2*i + 2; 
+
+        if (left < n && arr[left] > arr[largest])
+            largest = left;
+        if (right < n && arr[right] > arr[largest])
+            largest = right;
+        if (largest != i)
+        {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+            heapify(arr, n, largest);
+        }
     }
 
 
