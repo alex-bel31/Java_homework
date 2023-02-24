559c2af6f9e05c9c3b5b852689e6ff284fbe87ee
name <you@example.com>
2023-02-24 18:31:24 +0300

    heap building method and print array

--- homework_4/HeapSort.java
+++ homework_4/HeapSort.java
@@ -9,10 +9,27 @@ public class HeapSort{
     {
         int arr[] = fillArray();
         HeapSort ob = new HeapSort();
-       
+        String strSortArr = Arrays.toString(arr);
+        System.out.printf("Исходный массив: %s\n", strSortArr);
+        System.out.println("Отсортированный массив: ");
+        ob.sort(arr);
+        printArray(arr);
     }
 
+    public void sort(int arr[])
+    {
+        int n = arr.length;
 
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        for (int i=n-1; i>=0; i--)
+        {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, i, 0);
+        }
     }
 
      void heapify(int arr[], int n, int i)
@@ -34,6 +51,13 @@ public class HeapSort{
         }
     }
 
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
 
     public static int[] fillArray() {
         int[] arr = new int[10];
